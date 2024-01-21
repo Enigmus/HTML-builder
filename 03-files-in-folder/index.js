41566ca9e8af238c7e6fbcec7314cb6ea0a847e8
sd <sd_net@mail.ru>
2024-01-22 00:07:24 +0300

    refactor: удалил лишние переменные

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,22 +1,24 @@
 const fs = require('node:fs');
 const path = require('node:path');
-const { stdin, stdout, stderr } = process;
+const { stdout } = process;
 
-const secretFolder = 'secret-folder';
-const currentFolder = __dirname;
-const pathToSecretFolder = path.join(currentFolder, secretFolder);
+const pathToFolder = path.join(__dirname, 'secret-folder');
 
-fs.readdir(pathToSecretFolder, (err, files) => {
+fs.readdir(pathToFolder, (err, files) => {
   if (err) return stdout.write(err.message);
+
   files.forEach((file) => {
-    fs.stat(path.join(pathToSecretFolder, file), (er, st) => {
+    const pathToFile = path.join(pathToFolder, file);
+    fs.stat(pathToFile, (err, st) => {
       if (st.isDirectory()) return false;
       if (err) return stdout.write(err.message);
-      const objInf = path.parse(path.join(pathToSecretFolder, file));
-      const name = objInf.name;
-      const ext = objInf.ext.slice(1);
-      const size = (+st.size / 1024).toFixed(3);
-      stdout.write(`${name} - ${ext} - ${size}kb\n`);
+
+      const objInf = path.parse(pathToFile);
+      stdout.write(
+        `${objInf.name} - ${objInf.ext.slice(1)} - ${(st.size / 1024).toFixed(
+          3,
+        )}kb\n`,
+      );
     });
   });
 });
