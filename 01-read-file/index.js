c45d8ef38768b76b321e225481aafc3d940a41fb
sd <sd_net@mail.ru>
2024-01-24 21:07:01 +0300

    fix: up event

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -5,7 +5,7 @@ const stream = fs.createReadStream(path.join(__dirname, 'text.txt'), {
   encoding: 'utf-8',
 });
 
-stream.on('readable', () => console.log(stream.read()));
+stream.on('data', (data) => console.log(data));
 stream.on('error', (error) =>
   console.log(`Фигня какая то, вот ошибка ${error}`),
 );
